362923b12ad444ba9af1101fb3376e79f14b202c
xrudoga <defcw58@gmail.com>
2020-03-05 23:29:23 +0800

    Apply require signin tocreate post

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const { getPosts, createPost } = require('../controllers/post')
+const { requireSignin } = require('../controllers/auth')
+const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../Validator')
 
 const router = express.Router()
 
-router.get('/', getPosts)
-router.post('/post', createPostValidator, createPost)
+router.get('/', requireSignin, getPosts)
+router.post('/post', requireSignin, createPostValidator, createPost)
+
+// any route containing :userId our app will first execute userById()
+router.param('userId', userById)
 
 module.exports = router
