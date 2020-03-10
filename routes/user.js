49bdf2fb01007baaf4cb9d2c63cac7a0b9233a48
xrudoga <defcw58@gmail.com>
2020-03-11 00:14:44 +0800

    Find people component

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,17 @@ const {
     addFollowing,
     addFollower,
     removeFollowing,
-    removeFollower
+    removeFollower,
+    findPeople
 } = require('../controllers/user')
-const { requireSignin } = require('../controllers/auth')
 
+const { requireSignin } = require('../controllers/auth')
 const router = express.Router()
+
+
+// add following and follower
+router.put('/user/follow', requireSignin, addFollowing, addFollower)
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower)
 
 router.get('/users', allUsers)
 router.get('/user/:userId', requireSignin, getUser)
@@ -21,6 +27,8 @@ router.put('/user/:userId', requireSignin, updateUser)
 router.delete('/user/:userId', requireSignin, deleteUser)
 // photo
 router.get('/user/photo/:userId', userPhoto)
+// who to follow
+router.get('/user/findpeople/:userId', requireSignin, findPeople)
 
 router.put('/user/follow', requireSignin, addFollowing, addFollower)
 router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower)
