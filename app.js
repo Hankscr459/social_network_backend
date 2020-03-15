989c5eb7088489766eafee65d42e462a72419786
xrudoga <defcw58@gmail.com>
2020-03-15 17:06:20 +0800

    useCreateIndex

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ mongoose.connect(
     process.env.MONGO_URI,
         {   
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useCreateIndex: true,
+            useUnifiedTopology: true,
         },
     )
     .then(() => console.log('DB Connected'))
